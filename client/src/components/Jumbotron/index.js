480bd41fb108a15592ede8c1aa840ae997819ace
Tracy Collins <tracyecollins15@gmail.com>
2019-07-20 13:08:59 -0400

    changed text to white

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -3,7 +3,7 @@ import React from "react";
 function Jumbotron({ children }) {
   return (
     <div
-      style={{ height: 300, clear: "both", backgroundColor: "lightgreen", paddingTop: 120, textAlign: "center" }}
+      style={{ height: 300, clear: "both", backgroundColor: "lightgreen", paddingTop: 120, textAlign: "center", color: "white" }}
       className="jumbotron"
     >
       {children}
