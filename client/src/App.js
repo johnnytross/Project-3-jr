20999d7d48eb40110389679f9a375c10e8133d52
Ben Cason <bencason9@gmail.com>
2019-07-23 20:42:58 -0400

    added react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,35 @@
 import React from "react";
 import Books from "./pages/Books";
+import Saved from "./pages/Saved"
 import Nav from "./components/Nav";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
     <div>
-      <Nav />
-      <Books />
-    </div>
+    <Nav />
+    <Switch>
+    <Route path="/" component={Books} exact />
+    <Route path="/saved" component={Saved} />
+    <Route component={Error} />
+</Switch>
+</div>
+</BrowserRouter>
   );
 }
 
 export default App;
+
+/*<BrowserRouter>
+<div>
+<Navigation />
+<Switch>
+  <Route path="/" component={Home} exact />
+  <Route path="/about" component={About} />
+  <Route path="/contact" component={Contact} />
+  <Route component={Error} />
+</Switch>
+</div>
+</BrowserRouter>
+*/
---
+++ client/src/pages/Saved.js
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+import Jumbotron from "../components/Jumbotron";
+import API from "../utils/API";
+import DeleteBtn from "../components/DeleteBtn";
+import { Col, Row, Container } from "../components/Grid";
+import { List, ListItem } from "../components/List";
+import { Input, TextArea, FormBtn } from "../components/Form";
+import axios from 'axios'
+
+class Books extends Component {
+  state = {
+    books: []
+  };
+
+  componentDidMount() {
+    this.loadBooks();
+  }
+
+  loadBooks = () => {
+    API.getBooks()
+      .then(res => this.setState({ books: res.data }))
+      .catch(err => console.log(err));
+  };
+
+  handleChange = (e) => {
+    console.log(e)
+    console.log(e.target)
+    const { name, value } = e.target
+    console.log(name)
+    console.log(value)
+    this.setState({ [name]: value })
+  }
+
+  getRecipes = (e) => {
+    e.preventDefault();
+
+
+    console.log("getRecipes: " + this.state.recipe)
+    axios.get(`https://api.edamam.com/search?q=${this.state.recipe}&app_id=b3543550&app_key=318de1bc9554cc8c572774822aa601b4`)
+      .then(res => this.setState({ recipe: res.data }))
+      .catch(err => console.log(err));
+
+    console.log(this.state.recipe)
+
+  }
+
+
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="md-2">
+            <Jumbotron>
+              <h3>More stuff here...</h3>
+            </Jumbotron>
+
+          </Col>
+          <Col size="md-10">
+            <Jumbotron>
+              <h3>Saved Recipes</h3>
+            </Jumbotron>
+            {this.state.books.length ? (
+              <List>
+                {this.state.books.map(book => (
+                  <ListItem key={book._id}>
+                    <a href={"/books/" + book._id}>
+                      <strong>
+                        {book.title} by {book.author}
+                      </strong>
+                    </a>
+                    <DeleteBtn />
+                  </ListItem>
+                ))}
+              </List>
+            ) : (
+                <h5>No recipes found</h5>
+              )}
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Books;
