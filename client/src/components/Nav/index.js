5b381224e1bbc9b03154b2301d9d780d579bfc6b
Tracy Collins <tracyecollins15@gmail.com>
2019-07-23 21:03:12 -0400

    changed nav bar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 
 function Nav() {
   return (
-    <nav className="navbar navbar-dark bg-dark">
-      <a className="navbar-brand" href="/" style={{color: "white"}}>
+    <nav className='navbar navbar-dark bg-dark'>
+      <a className='navbar-brand' href='/' style={{ color: 'white' }}>
         Veganize
       </a>
-      <a className="navbar-brand" href="/saved" style={{color: "white"}}>
+      <a className='navbar-brand' href='/saved' style={{ color: 'white' }}>
         Saved Recipes
       </a>
     </nav>
