23255aaaf383a227c2d24aed9ad87ea1085987c9
Tracy Collins <tracyecollins15@gmail.com>
2019-07-25 14:11:26 -0400

    new page added for login

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
-import React from "react";
-import Books from "./pages/Books";
-import Saved from "./pages/Saved";
-import About from "./pages/About";
-import Nav from "./components/Nav";
+import React from 'react';
+import Books from './pages/Books';
+import Saved from './pages/Saved';
+import About from './pages/About';
+import Nav from './components/Nav';
+import Login from './pages/Login';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
@@ -11,9 +12,10 @@ function App() {
       <div>
         <Nav />
         <Switch>
-          <Route path="/" component={Books} exact />
-          <Route path="/saved" component={Saved} />
-          <Route path="/about" component={About} />
+          <Route path='/' component={Books} exact />
+          <Route path='/login' component={Login} exact />
+          <Route path='/saved' component={Saved} />
+          <Route path='/about' component={About} />
           <Route component={Error} />
         </Switch>
       </div>
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 
 function Nav() {
   return (
-    <nav className='navbar navbar-dark bg-dark'>
-      <a className='navbar-brand' href='/' style={{ color: 'white' }}>
-        Veganize
-      </a>
-      <a className='navbar-brand' href='/saved' style={{ color: 'white' }}>
-        Saved Recipes
-      </a>
-      <a className="navbar-brand" href="/about" style={{color: "white"}}>
-        About
-      </a>
+    <nav className='navbar navbar-dark teal lighten-2' style={{ opacity: 0.8 }}>
+      <div>
+        <a className='navbar-brand' href='/' style={{ color: 'white' }}>
+          Veganize
+        </a>
+        <a className='navbar-brand' href='/saved' style={{ color: 'white' }}>
+          Saved Recipes
+        </a>
+        <a className='navbar-brand' href='/about' style={{ color: 'white' }}>
+          About
+        </a>
+      </div>
+      <div>
+        <a className='navbar-brand' href='/login' style={{ color: 'white' }}>
+          Login
+        </a>
+      </div>
     </nav>
   );
 }
