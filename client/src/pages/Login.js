0d5f85cd8cf4ffcf5555ce89b6cac6b07fe94893
Tracy Collins <tracyecollins15@gmail.com>
2019-08-03 14:15:23 -0400

    latest changes to authentication

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,38 +1,12 @@
 import React, { Component } from 'react';
 import LoginForm from '../components/Login';
-import Jumbotron from '../components/Jumbotron';
-// import API from "../utils/API";
-// import DeleteBtn from "../components/DeleteBtn";
-import { Col, Row, Container } from '../components/Grid';
-// import { List, ListItem } from "../components/List";
-import { Input } from '../components/Form';
-// import axios from 'axios';
+import { Container } from '../components/Grid';
 
 class Login extends Component {
   render() {
     return (
       <Container fluid>
-        <Row>
-          <Col size='md-12'>
-            <Jumbotron>
-              <h3>Login</h3>
-              <br />
-              <Row>
-                <Col size='md-3' />
-                <Col size='md-6'>
-                  <Input />
-                  <Input />
-                  <br />
-                  <input
-                    className='btn btn-success btn-success btn-lg'
-                    type='submit'
-                    value='Submit'
-                  />
-                </Col>
-              </Row>
-            </Jumbotron>
-          </Col>
-        </Row>
+        <LoginForm />
       </Container>
     );
   }
